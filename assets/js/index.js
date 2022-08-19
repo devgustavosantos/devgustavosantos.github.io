5ab80076ba3aa84d412114c11cc9f38a575b57f4
gustavo santos <devgustavosantos@gmail.com>
2022-08-19 13:30:16 -0300

    feat: creates interactivity with the projects slide

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,107 @@ function showAllProjects() {
 
 buttonMenu.addEventListener("click", toggleMenu);
 buttonShowMore.addEventListener("click", showAllProjects);
+
+// buttonNext.addEventListener("click", nextProject);
+
+// Funções para versão--------------------------------------------------------
+function appVersion() {
+    const windowsWidth = window.screen.width;
+
+    if (windowsWidth > 600) {
+        showDesktopVersion();
+    } else {
+        showMobileVersion();
+    }
+}
+
+function showDesktopVersion() {
+    const projects = document.querySelectorAll(".project");
+
+    projects.forEach(project => project.classList.remove("hidden"));
+}
+
+function showMobileVersion() {
+    showOnlyThreeProjects();
+    buttonShowMore.classList.remove("hidden");
+}
+
+function showOnlyThreeProjects() {
+    const projects = Array.from(document.querySelectorAll(".project"));
+    for (let cont in projects) {
+        if (cont <= 2) {
+            projects[cont].classList.remove("hidden");
+        } else {
+            projects[cont].classList.add("hidden");
+        }
+    }
+}
+
+window.addEventListener("resize", appVersion);
+
+appVersion();
+
+// Funções para Slider -------------------------------------------------------
+const sliderControls = document.querySelectorAll(".control-slider");
+let currentProjects;
+const projects = document.querySelectorAll(".project");
+const maxProjects = projects.length;
+
+sliderControls.forEach(control =>
+    control.addEventListener("click", slideProject)
+);
+
+function slideProject(e) {
+    //Para o slider funcionar como o esperado, é necessário mover a tela um px
+    moveTheScreen();
+
+    const isLeftControl = e.currentTarget.classList.contains("arrow-left");
+
+    if (isLeftControl) {
+        currentProjects -= 1;
+    } else {
+        currentProjects += 1;
+    }
+
+    if (currentProjects >= maxProjects) {
+        currentProjects = 0;
+    }
+
+    if (currentProjects < 0) {
+        currentProjects = maxProjects - 1;
+    }
+
+    projects[currentProjects].scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+    });
+
+    projects.forEach(project =>
+        project.children[2].classList.remove("current-project")
+    );
+    projects[currentProjects].children[2].classList.add("current-project");
+}
+
+function moveTheScreen() {
+    const currentPosition = window.scrollY;
+    window.scrollTo(0, currentPosition + 1);
+}
+
+function projectsDisplayed() {
+    const windowsWidth = window.screen.width;
+
+    if (windowsWidth < 800) {
+        currentProjects = 1;
+    } else {
+        currentProjects = 2;
+    }
+}
+
+function highlightExhibitedProjects() {
+    projects.forEach(project =>
+        project.children[2].classList.add("current-project")
+    );
+}
+
+projectsDisplayed();
+highlightExhibitedProjects();
