deae4533d20654a63bfaecef5fbb0e224032404d
gustavo santos <devgustavosantos@gmail.com>
2022-08-19 16:16:01 -0300

    refactor: modularized the interactivity of the project

---
+++ assets/js/elements.js
@@ -0,0 +1,8 @@
+const buttonMenu = document.querySelector("header button");
+const buttonShowMore = document.querySelector(".show-more");
+
+const projects = document.querySelectorAll(".project");
+
+const sliderControls = document.querySelectorAll(".control-slider");
+
+export { buttonMenu, buttonShowMore, projects, sliderControls };
